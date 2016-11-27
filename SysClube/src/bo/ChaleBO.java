6ff8d6a49e405a0fda4c7a292baeebe2a767e77f
Gui Freitas <Luiz Freitas@DESKTOP>
2016-11-27 09:02:07 -0200

    Criação do BO chalé

---
+++ SysClube/src/bo/ChaleBO.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bo;
+
+/**
+ *
+ * @author Gui Freitas
+ */
+public class ChaleBO {
+    //Atributos
+    protected int cod_chale;
+    protected String desc_chale;
+    
+    public ChaleBO(int cod_chale, String desc_chale){
+        this.cod_chale = cod_chale;
+        this.desc_chale = desc_chale;
+    }
+    
+    //Metodos
+    public int getCod_chale() {
+        return cod_chale;
+    }
+
+    public void setCod_chale(int cod_chale) {
+        this.cod_chale = cod_chale;
+    }
+
+    public String getDesc_chale() {
+        return desc_chale;
+    }
+
+    public void setDesc_chale(String desc_chale) {
+        this.desc_chale = desc_chale;
+    }
+    
+}
