9451c072bbb28b82146513abca25f9083258abef
Gui Freitas <>
2016-11-27 08:55:51 -0200

    Primeiro commit

---
+++ SysClube/src/bo/UsuarioBO.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bo;
+
+/**
+ *
+ * @author Gui Freitas
+ */
+public class UsuarioBO {
+
+    private int matricula;
+    private String cpf;
+    private String nome;
+    private String acesso;
+    private String senha;
+    private int status;
+
+    /**
+     * @return the matricula
+     */
+    public int getMatricula() {
+        return matricula;
+    }
+
+    /**
+     * @param matricula the matricula to set
+     */
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    /**
+     * @return the cpf
+     */
+    public String getCpf() {
+        return cpf;
+    }
+
+    /**
+     * @param cpf the cpf to set
+     */
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the acesso
+     */
+    public String getAcesso() {
+        return acesso;
+    }
+
+    /**
+     * @param acesso the acesso to set
+     */
+    public void setAcesso(String acesso) {
+        this.acesso = acesso;
+    }
+
+    /**
+     * @return the senha
+     */
+    public String getSenha() {
+        return senha;
+    }
+
+    /**
+     * @param senha the senha to set
+     */
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+    
+    public void vlrStatus(int valor){
+        
+    }
+
+
+}
